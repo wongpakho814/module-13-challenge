1156dd56f01d3892bdeac4959a4723403ebba920
wongpakho814 <wongpakho814@gmail.com>
2022-11-10 18:15:04 +1100

    Updated tag-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -53,15 +53,16 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
+
     if (!categoryData[0]) {
       res.status(404).json({ message: "No category with this id!" });
       return;
     }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 router.delete("/:id", async (req, res) => {
@@ -77,11 +78,11 @@ router.delete("/:id", async (req, res) => {
       res.status(404).json({ message: "No category found with that id!" });
       return;
     }
+    
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 module.exports = router;
